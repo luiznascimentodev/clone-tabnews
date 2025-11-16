a7396606c6a510d5ccfef33cfc7fe27933337062
Luiz Nascimento <luiznascdev@gmail.com>
2025-11-16 00:49:27 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1>Estou fazendo curso.dev para aprimorar meus conhecimentosg</h1>
+function Home() {
+  return <h1>Estou fazendo curso.dev para aprimorar meus conhecimentosg</h1>;
 }
 
 export default Home;
