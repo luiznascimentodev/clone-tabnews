4077f61d139b999d2af19694684f18573f205616
Luiz Nascimento <luiznascdev@gmail.com>
2025-11-17 16:07:22 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const maxConnectionQuery = await database.query("SHOW max_connections;");
+  const maxConnection = maxConnectionQuery.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionQuery = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;", // Corrigido
+    values: [databaseName],
+  });
+  const openedConnection = openedConnectionQuery.rows[0].count;
+
+  const postgresQuery = await database.query("SHOW server_version;");
+  const postgresVersion = postgresQuery.rows[0].server_version;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: parseInt(maxConnection),
+        opened_connections: openedConnection,
+      },
+    },
+  });
 }
 
 export default status;
